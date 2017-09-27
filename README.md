d967480f6fb5a94d878afbf64507c51a9151e274
pedrojosepajares <pedrojosepajares@gmail.com>
2017-09-27 18:04:09 +0200

    Base de datos creada

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # full-stack-example
 Ejemplo donde una base de datos, un cliente y un servidor interactúan. El entorno de desarrollo es Python. También se ha utilizado Flask.
+
+Prerrequisitos:
+
+1. SQLite: Base de datos SQL simple
+	- Decargar "sqlite-autoconf-3200100.tar.gz" desde https://www.sqlite.org/download.html
+	- tar xvfz sqlite-autoconf-3200100.tar.gz
+	- cd sqlite-autoconf-3200100/
+	- ./configure
+	- make
+	- sudo make install
+	- cd ..
+	- rm -rf  sqlite-autoconf-3200100 sqlite-autoconf-3200100.tar.gz
+
+2. Python: instalado por defecto en las distribuciones de Ubuntu
+
+3. SQLAlchemy: Object-Relational Mapping (ORM) para Python
+	- Instalación de pip: sudo apt install python-pip -y
+	- pip install SQLAlchemy
+
+
+
+
---
+++ database_setup.py
@@ -0,0 +1,38 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Clases
+class Restaurant(Base):
+    #Informacion de la tabla  
+    __tablename__ = "restaurant"
+    
+    #Mappers
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+class MenuItem(Base):  
+    #Informacion de la tabla  
+    __tablename__ = "menu_item"
+
+    #Mappers
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    restaurant = relationship(Restaurant)
+
+
+
+
+
+engine = create_engine("sqlite:///restaurantmenu.db")
+# Creacion del fichero restaurantmenu.db
+Base.metadata.create_all(engine)
