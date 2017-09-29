df84095e565df96f79b411640640823f2b6baccc
pedrojosepajares <pedrojosepajares@gmail.com>
2017-09-29 21:46:35 +0200

    POST y GET en Flask

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -16,12 +16,35 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
-    output = ''
-    for item in items:
-        output += item.name
-        output += '<br>'
-    return output
+@app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/',  methods = ['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    if request.method == 'POST':
+        item = session.query(MenuItem).filter_by(id = menu_id).one()
+        item.name = request.form['name']
+        session.add(item)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id)
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(id = menu_id).one()
+    session.delete(item)
+    session.commit()
+    return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 
 if __name__ == '__main__':
     app.debug = True
